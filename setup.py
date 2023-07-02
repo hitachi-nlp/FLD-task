4f1d97859bf4af31c72916127cf3ab845346a664
honoka <hello@hello_world.com>
2023-07-02 20:38:45 +0900

    [update] library name

--- setup.py
+++ setup.py
@@ -2,12 +2,9 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(
-    name='FLD task',
+    name='FLD_task',
     version='0.0',
     description='A utility library for loading FLD datasets and computing metrics on them.',
-    author='Honoka',
-    author_email='terufumi.morishita.wp@hitachi.com',
-    license='Hitachi R&D',
     install_requires=[
         'nltk',
         'evaluate',
