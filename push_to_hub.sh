25c3f60eefd8f3ac533445e26a4556ed5aa58bd8
MorishT <magi.magilmagi@gmail.com>
2023-11-10 20:55:44 +0900

    [update] doc

--- push_to_hub.sh
+++ push_to_hub.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-
+# XXX!!! Before pushing to hub, be sure to checkout the version that match the remote repo,
+# as the push script add some features to the dataset.
 
 # -- FLD.v2 --
 # TEST=./outputs.FLD/00.create_corpus/20230801.case_study_finalize.fix/dataset_name=20230729.case_study_finalize.D3/trnsltn_adj_vrb_nn_rt=1-1-1/test/test.jsonl
