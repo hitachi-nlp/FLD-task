92814ee922566794d6eb4867e1714c1c3223a882
MorishT <magi.magilmagi@gmail.com>
2024-01-24 21:02:08 +0900

    [WARNING] try loading as lower versions if the version is DeductionInstance

--- FLD_task/loader.py
+++ FLD_task/loader.py
@@ -1,9 +1,17 @@
 import re
 from copy import copy
+import logging
 
+from pydantic.error_wrappers import ValidationError
 from typing import Union, Optional, Callable, Any
+
 from FLD_task.schema import Deduction
 from FLD_task.proof.utils import FACT_IDENT, FACTS_IDENT
+
+logger = logging.getLogger(__name__)
+
+
+_NUMBERD_VERSIONS = ['0.0', '0.1', '0.2', '0.3']
 
 
 def load_deduction(dic: dict, force_version: str = None) -> Deduction:
@@ -28,72 +36,84 @@ def load_deduction(dic: dict, force_version: str = None) -> Deduction:
     def rename_fact_ident(rep: str) -> str:
         return re.sub(r'sent([0-9]+)', f'{FACT_IDENT}\g<1>', rep)
 
-    if version in ['0.0', '0.1', '0.2']:
+    if version in _NUMBERD_VERSIONS:
 
-        map_to_new_field('context', f'{FACTS_IDENT}', convert_func=rename_fact_ident)
-        map_to_new_field('context_formula', f'{FACTS_IDENT}_formula', convert_func=rename_fact_ident)
-        map_to_new_field('proofs', 'proofs', convert_func = lambda proofs: [rename_fact_ident(p) for p in proofs])
-        map_to_new_field('proofs_formula', 'proofs_formula', convert_func = lambda proofs: [rename_fact_ident(p) for p in proofs])
-        map_to_new_field('negative_proofs', 'negative_proofs', convert_func = lambda proofs: [rename_fact_ident(p) for p in proofs])
-        map_to_new_field('negative_proofs_formula', 'negative_proofs_formula', convert_func = lambda proofs: [rename_fact_ident(p) for p in proofs])
+        if version in ['0.0', '0.1', '0.2']:
 
-        if version in ['0.0', '0.1']:
-            if version == '0.0':
-                def convert_answer(ans: Union[bool, str]) -> str:
-                    if ans is True:
-                        return 'PROVED'
-                    elif ans is False:
-                        return 'DISPROVED'
-                    elif ans == 'Unknown':
-                        return 'UNKNOWN'
-                    else:
-                        raise ValueError(f'Unknown answer {ans}')
+            map_to_new_field('context', f'{FACTS_IDENT}', convert_func=rename_fact_ident)
+            map_to_new_field('context_formula', f'{FACTS_IDENT}_formula', convert_func=rename_fact_ident)
+            map_to_new_field('proofs', 'proofs', convert_func = lambda proofs: [rename_fact_ident(p) for p in proofs])
+            map_to_new_field('proofs_formula', 'proofs_formula', convert_func = lambda proofs: [rename_fact_ident(p) for p in proofs])
+            map_to_new_field('negative_proofs', 'negative_proofs', convert_func = lambda proofs: [rename_fact_ident(p) for p in proofs])
+            map_to_new_field('negative_proofs_formula', 'negative_proofs_formula', convert_func = lambda proofs: [rename_fact_ident(p) for p in proofs])
 
-                def convert_stance(stance: str) -> str:
-                    if stance == 'PROOF':
-                        return 'PROVED'
-                    elif stance == 'DISPROOF':
-                        return 'DISPROVED'
-                    elif stance == 'UNKNOWN':
-                        return 'UNKNOWN'
-                    else:
-                        raise ValueError(f'Unknown stance {stance}')
-            elif version == '0.1':
-                convert_answer = None
-                convert_stance = None
+            if version in ['0.0', '0.1']:
+                if version == '0.0':
+                    def convert_answer(ans: Union[bool, str]) -> str:
+                        if ans is True:
+                            return 'PROVED'
+                        elif ans is False:
+                            return 'DISPROVED'
+                        elif ans == 'Unknown':
+                            return 'UNKNOWN'
+                        else:
+                            raise ValueError(f'Unknown answer {ans}')
+
+                    def convert_stance(stance: str) -> str:
+                        if stance == 'PROOF':
+                            return 'PROVED'
+                        elif stance == 'DISPROOF':
+                            return 'DISPROVED'
+                        elif stance == 'UNKNOWN':
+                            return 'UNKNOWN'
+                        else:
+                            raise ValueError(f'Unknown stance {stance}')
+                elif version == '0.1':
+                    convert_answer = None
+                    convert_stance = None
+                else:
+                    raise ValueError()
+
+                map_to_new_field('answer', 'world_assump_label', convert_func=convert_answer)
+                map_to_new_field('negative_answer', 'negative_world_assump_label', convert_func=convert_answer)
+
+                map_to_new_field('proof_stance', 'proof_label', convert_func=convert_stance)
+                map_to_new_field('negative_proof_stance', 'negative_proof_label', convert_func=convert_stance)
+
+            elif version == '0.2':
+                pass
+
             else:
-                raise ValueError()
+                raise Exception()
 
-            map_to_new_field('answer', 'world_assump_label', convert_func=convert_answer)
-            map_to_new_field('negative_answer', 'negative_world_assump_label', convert_func=convert_answer)
-
-            map_to_new_field('proof_stance', 'proof_label', convert_func=convert_stance)
-            map_to_new_field('negative_proof_stance', 'negative_proof_label', convert_func=convert_stance)
-
-        elif version == '0.2':
+        elif version in ['0.3']:
             pass
 
         else:
-            raise Exception()
-
-    elif version in ['0.3']:
-        pass
+            raise ValueError(f'Unknown version {version}')
 
     elif version in ['DeductionInstance', 'DeductionExampleInstance']:
-        pass
+
+        # Guess the original version before loaded as DeductionInstance
+        version_guess_deduction = None
+        for possible_versions in sorted(_NUMBERD_VERSIONS)[::-1]:
+            try:
+                version_guess_deduction = load_deduction(dic, force_version=possible_versions)
+                break
+            except ValidationError as e:
+                # logger.info(f'Failed to load deduction as version {possible_versions} with the following error:\n{e}')
+                pass
+        if version_guess_deduction is None:
+            raise ValueError('Failed to load deduction as any of the versions')
+        else:
+            # logger.info(f'Successfully loaded deduction as version {possible_versions}')
+            dic = version_guess_deduction.dict()
 
     else:
         raise ValueError()
 
     dic['version'] = 'DeductionInstance'
     init_kwargs = {key: val for key, val in dic.items() if val is not None}
-
-    # We could check the unnecessary fields,
-    # but it could be useful if we ignore such fields when we are loading examples from 
-    # modified jsons or something.
-    # for key in init_kwargs:
-    #     if key not in Deduction.schema()['properties']:
-    #         raise Exception(f'key={key} not allowed for Deduction object')
 
     deduction = Deduction.parse_obj(init_kwargs)
     return deduction
